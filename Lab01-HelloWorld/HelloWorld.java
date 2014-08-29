30c38c9fea97947b5e72d7f4c71b8bb994c642e2
ant215 <ant215@lehigh.edu>
2014-08-29 13:36:08 +0000

    updated HelloWorld

---
+++ Lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////
+//Anil Tipirneni
+//Lab 01
+//Hello World Java Program
+//
+//
+// Define a Class
+public class HelloWorld{
+    
+    
+    // Add main method
+    public static void main(String[] args) {
+        
+        // Print boxed statement
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("##                   ##");
+        System.out.println("## Hello World 2014! ##");
+        System.out.println("##                   ##");
+        System.out.println("#######################");
+        System.out.println("#######################");
+    }
+}
+
