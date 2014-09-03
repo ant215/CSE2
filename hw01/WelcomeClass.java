cb5b78672b8d01c27d4fa91fd4f69888e8a04cde
ant215 <ant215@lehigh.edu>
2014-09-03 22:32:33 +0000

    hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+///////////////////////////////////////////////////////
+//Anil Tipirneni
+//Hw 01
+//Welcome Class Java Program
+//
+//
+// Define a Class
+public class WelcomeClass{
+    
+    
+    // Add main method
+    public static void main(String[] args) {
+        
+        // Print boxed statement
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println(" ^ ^ ^ ^ ^ ^ ^");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-A-N-T-2-1-5-|");
+        System.out.println("\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println(" v  v  v  v  v ");
+        System.out.println("This past weekend I went to Flushing Meadows, NY to go to the US Open.\nI was able to watch one of my friends play againt the number 1 ranked\ndoubles team in world.");
+    }
+}
